d1ebb1dadda0a36e213b8bb4da58edb25ef5286c
josepa14 <jcasper2112@g.educaand.es>
2023-02-09 14:16:13 +0100

    mejoras mesa

--- T3 JQUERY/ejercicio 3 mesas proyecto/JS/js.js
+++ T3 JQUERY/ejercicio 3 mesas proyecto/JS/js.js
@@ -1,24 +1,49 @@
 //servidor trae mesas y yo creo en modo objeto
 //pinto mesas, en almacen
 $(function () {
-  var mesa = new Mesa()
-  $('#agregar').click(function () {
-    mesa.pinta();
+//porgramacion eficiente meter en variables sala y almacen y pasarselo 
+//a pinta para que no se tenga que buscar cadave que se ejecuta al mismo tiempo que se pueden cambiar en la lineas de aqui
+  //ajax de la api
+
+//este codigo debe borrarse cuando desaparezcan las mesas añadidas asangre
+  $(".mesa").draggable({
+    start: function (ev, ui) {
+      $(this).attr("data-y", ui.offset.top)
+      $(this).attr("data-x", ui.offset.left)
+    }, revert: true, helper: "clone", revertDuration: 0
   })
-
-  //control de movimiento
-  mesa.movimiento();
-
-
-
+//este codigo esta bien aqui
   $("#almacen").droppable({
     drop: function (ev, ui) {
-     let mesaAlmacen = ui.draggable;
-     mesaAlmacen.attr("style", "");
+      let mesaAlmacen = ui.draggable;
+      mesaAlmacen.attr("style", "");
       $(this).append(mesaAlmacen)
-
     }
+  })
+
+
+  $('.dialog').hide();
+
+
+  $("#anadir").click(function () {
+    //capturamos los valores de la altura y anchura de la mesa
+    altmesa = $("#altura").val();
+    anchmesa = $("#anchura").val();
+    console.log(altmesa);
+    console.log(anchmesa);
+    //cerramos el dialog
+    $(".dialog").dialog("close");
+    var mesa = new Mesa(altmesa, anchmesa)
+    mesa.pinta();
   });
+
+  $('#agregar').click(function () {
+    $(".dialog").dialog({ autoOpen: true });
+
+  }
+  )
+  //control de movimiento
+
 
   $(".sala").droppable({
     drop: function (ev, ui) {
@@ -43,41 +68,55 @@ $(function () {
 
 
       //$.each(function(ev,i){
-        console.log('hola')
-        var mesaYa = $(".sala .mesa").eq(0);
+
+      var mesaYa = $(".sala .mesa");
       console.log(mesaYa)
+
+
       if (mesaYa.length > 0) {
+        var choca = false;
 
-        var posX = parseInt(mesaYa.offset().left)
-        var posY = parseInt(mesaYa.offset().top)
-        var anchura = parseInt(mesaYa.width())
-        var altura = parseInt(mesaYa.height())
-        var pos2 = [posX, posX + anchura, posY, posY + altura];
+        mesaYa.each(function (i, v) {
 
-        if (((pos1[0] > pos2[0] && pos1[0] < pos2[1]) ||
-          (pos1[1] > pos2[0] && pos1[1] < pos2[1]) ||
-          (pos1[0] <= pos2[0] && pos1[1] >= pos2[1])) &&
-          ((pos1[2] > pos2[2] && pos1[2] < pos2[3]) ||
-            (pos1[3] > pos2[2] && pos1[3] < pos2[3]) ||
-            (pos1[2] <= pos2[2] && pos1[3] >= pos2[3]))) {
-          console.log("CHOCA")
-        } else {
+          var mesaPrueba = $(v);
+          if (mesaPrueba.attr('id') != undefined && mesa.attr('id') != mesaPrueba.attr('id')) {//dos formas hacer estamos mezclando elementos jquery y javascript
+            var posX = parseInt(mesaPrueba.offset().left)
+            var posY = parseInt(mesaPrueba.offset().top)
+            var anchura = parseInt(mesaPrueba.width())
+            var altura = parseInt(mesaPrueba.height())
+            var pos2 = [posX, posX + anchura, posY, posY + altura];
+            if (((pos1[0] > pos2[0] && pos1[0] < pos2[1]) ||
+              (pos1[1] > pos2[0] && pos1[1] < pos2[1]) ||
+              (pos1[0] <= pos2[0] && pos1[1] >= pos2[1])) &&
+              ((pos1[2] > pos2[2] && pos1[2] < pos2[3]) ||
+                (pos1[3] > pos2[2] && pos1[3] < pos2[3]) ||
+                (pos1[2] <= pos2[2] && pos1[3] >= pos2[3]))) {
+
+              choca = true;
+            }
+          }
+        })
+        debugger
+        if (!choca) {
+
           $(this).append(mesa)
           mesa.css({ position: "absolute", top: top - difY + "px", left: left - difX + "px" })
         }
+
+
       } else {
         $(this).append(mesa)
         mesa.css({ position: "absolute", top: top - difY + "px", left: left - difX + "px" })
       }
       //})
-      
+
     }
   }
   );
 
 
-})
 
-//crear objeto mesa
-//crear objeto sala
-//crear objeto almacen
+  //crear objeto mesa
+  //crear objeto sala
+  //crear objeto almacen
+})
--- T3 JQUERY/ejercicio 3 mesas proyecto/JS/mesa.js
+++ T3 JQUERY/ejercicio 3 mesas proyecto/JS/mesa.js
@@ -1,8 +1,9 @@
-function Mesa(posy,posx,alto,ancho){
+function Mesa(alto,ancho,posy,posx){
     this.posicionX = posx
     this.posicionY = posy
-    this.ancho = alto
-    this.alto = ancho
+    this.ancho = ancho
+    this.alto = alto
+   // this.id= 1;
 }
 
 Mesa.prototype.posicion=function(){
@@ -11,17 +12,44 @@ Mesa.prototype.posicion=function(){
 }
 
 Mesa.prototype.pinta=function(){
+   // this.id = this.id+1
+    var almesa = this.alto;
+    var ancMesa= this.ancho;
+
     var div = $('<div>');
-    div.addClass("mesa").appendTo('.almacen')
-    div.draggable()
+    //sala posicion abosluta alamacen relativo
+    div.addClass("mesa").appendTo('.almacen').css({"height":almesa,"width": ancMesa})
+    div.draggable(
+        {
+        start: function (ev, ui) {
+          $(this).attr("data-y", ui.offset.top)
+          $(this).attr("data-x", ui.offset.left)
+          $(this).attr("id", this.id)
+        }, revert: true, helper: "clone", revertDuration: 0
+      }
+      )
     console.log(div)
 }
-
-Mesa.prototype.movimiento=function(){
+//esto es un comodin para las mesas que he creado a sangre
+movimiento=function(){
     $(".mesa").draggable({
         start: function (ev, ui) {
           $(this).attr("data-y", ui.offset.top)
           $(this).attr("data-x", ui.offset.left)
         }, revert: true, helper: "clone", revertDuration: 0
       });
+}
+almacenaje=function(){
+    $("#almacen").droppable({
+        drop: function (ev, ui) {
+         let mesaAlmacen = ui.draggable;
+         mesaAlmacen.attr("style", "");
+          $(this).append(mesaAlmacen)
+    
+        }
+      });
+}
+Mesa.prototype.choca=function(otraMesa){
+
+    return false;
 }
